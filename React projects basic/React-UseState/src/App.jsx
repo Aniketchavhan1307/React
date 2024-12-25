166e246428f5b86f6d7e1361a4dedbc117774ef0
Aniket <saniketchavhan123@gmail.com>
2024-12-25 15:51:54 +0530

    Add initial useState project: Basic counter app implementation.

---
+++ React projects basic/React-UseState/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { use } from 'react'
+import Count from './components/count.jsx'
+
+
+function App() {
+  const[count, setCount] = useState(0);
+
+  return (
+    
+   <div className='container'>
+    <p className='para'> The count is {count}</p>
+    <button className='btn' onClick={() =>setCount(count+1)} >Click me</button>
+   
+   <div>
+    <Count name="Aniket">
+    <h1 className='title'>Aniket Chavhan</h1>
+    <p className='description'>I am a software engineer.</p>
+
+    </Count>
+   </div>
+   </div>
+  )
+}
+
+export default App
---
+++ React projects basic/React-UseState/src/components/Count.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const count = (props) => {
+  return (
+    <div >
+        {props.children}
+        
+    </div>
+  )
+}
+
+export default count
