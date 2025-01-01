d54e21747b01327c43673290925fb7fa41355a99
Aniket <saniketchavhan123@gmail.com>
2025-01-01 15:01:47 +0530

    completed our Paste Application .

--- paste-app/src/components/Home.jsx
+++ paste-app/src/components/Home.jsx
@@ -1,93 +1,254 @@
-import React, { useEffect, useState } from 'react'
-import { useSearchParams } from 'react-router';
-import { addToPastes, updateToPastes } from '../redux/pasteSlice';
-import Paste from './Paste';
-import { useDispatch, useSelector } from 'react-redux'
+import { Copy, PlusCircle } from "lucide-react";
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
+import { useDispatch, useSelector } from "react-redux";
+import { addToPastes, updatePastes } from "../redux/pasteSlice";
+import { useSearchParams } from "react-router-dom";
 
 const Home = () => {
-    const [title, setTitle] = useState('');
-    const [value, setValue] = useState('');
-    const [searchParam, setSearchParam] = useSearchParams();
-    const pasteId = searchParam.get('pasteId');
-    const dispatch = useDispatch();
-    const pastes= useSelector((state)=> state.paste.pastes)
+  const [value, setValue] = useState("");
+  const [title, setTitle] = useState("");
+  const [searchParams, setSearchParams] = useSearchParams(); // Destructure useSearchParams
+  const pasteId = searchParams.get("pasteId"); // Get pasteId from the search params
+  const pastes = useSelector((state) => state.paste.pastes);
+  const dispatch = useDispatch()
+
+  const createPaste = () => {
+    const paste = {
+      title: title,
+      content: value,
+      _id:
+        pasteId ||
+        Date.now().toString(36) + Math.random().toString(36).substring(2),
+      createdAt: new Date().toISOString(),
+    };
+
+    if (pasteId) {
+      // If pasteId is present, update the paste
+      dispatch(updatePastes(paste));
+    } else {
+      dispatch(addToPastes(paste));
+    }
+
+    setTitle("")
+    setValue("");
+
+    // Remove the pasteId from the URL after creating/updating a paste
+    setSearchParams({});
+  };
+
+  const resetPaste = () => {
+    setTitle("");
+    setValue("");
+    setSearchParams({});
+    // navigate("/");
+  };
+
+  useEffect(() => {
+    if (pasteId) {
+      const paste = pastes.find((p) => p._id === pasteId);
+      if (paste) {
+        setTitle(paste.title);
+        setValue(paste.content);
+      }
+    }
+  }, [pasteId, pastes])
+
+
+  return (
+    <div className="w-full h-full py-10 max-w-[1200px]  mx-auto px-5 lg:px-0">
+      <div className="flex flex-col gap-y-5 items-start">
+        <div className="w-full flex flex-row gap-x-4 justify-between items-center">
+          <input
+            type="text"
+            placeholder="Title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            // Dynamic width based on whether pasteId is present
+            className={`${
+              pasteId ? "w-[80%]" : "w-[85%]"
+            }   text-white border  border-input rounded-md p-2`}
+          />
+          <button
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700"
+            onClick={createPaste}
+          >
+            {pasteId ? "Update Paste" : "Create My Paste"}
+          </button>
+
+        {pasteId &&  <button
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700"
+            onClick={resetPaste}
+          >
+            <PlusCircle size={20} />
+          </button>}
+        </div>
+
+        <div
+          className={`w-full flex flex-col items-start relative rounded bg-opacity-10 border border-[rgba(128,121,121,0.3)] backdrop-blur-2xl`}
+        >
+          <div
+            className={`w-full rounded-t flex items-center justify-between gap-x-4 px-4 py-2 border-b border-[rgba(128,121,121,0.3)]`}
+          >
+            <div className="w-full flex gap-x-[6px] items-center select-none group">
+              <div className="w-[13px] h-[13px] rounded-full flex items-center justify-center p-[1px] overflow-hidden bg-[rgb(255,95,87)]" />
+
+              <div
+                className={`w-[13px] h-[13px] rounded-full flex items-center justify-center p-[1px] overflow-hidden bg-[rgb(254,188,46)]`}
+              />
+
+              <div className="w-[13px] h-[13px] rounded-full flex items-center justify-center p-[1px] overflow-hidden bg-[rgb(45,200,66)]" />
+            </div>
+            {/* Circle and copy btn */}
+            <div
+              className={`w-fit rounded-t flex items-center justify-between gap-x-4 px-4`}
+            >
+              {/*Copy  button */}
+              <button
+                className={`flex justify-center items-center  transition-all duration-300 ease-in-out group`}
+                onClick={() => {
+                  navigator.clipboard.writeText(value);
+                  toast.success("Copied to Clipboard", {
+                    position: "top-right",
+                  });
+                }}
+              >
+                <Copy className="group-hover:text-success-500" size={20} />
+              </button>
+            </div>
+          </div>
+
+          {/* TextArea */}
+          <textarea
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+            placeholder="Write Your Content Here...."
+            className="w-full p-3  focus-visible:ring-0"
+            style={{
+              caretColor: "#000",
+            }}
+            rows={20}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { useEffect, useState } from 'react'
+// import { useSearchParams } from 'react-router';
+// import { addToPastes, updateToPastes } from '../redux/pasteSlice';
+// import Paste from './Paste';
+// import { useDispatch, useSelector } from 'react-redux'
+
+// const Home = () => {
+//     const [title, setTitle] = useState('');
+//     const [value, setValue] = useState('');
+//     const [searchParam, setSearchParam] = useSearchParams();
+//     const pasteId = searchParam.get('pasteId');
+//     const dispatch = useDispatch();
+//     const pastes= useSelector((state)=> state.paste.pastes)
 
 
    
     
 
-    const createPaste = ()=>
-    {
-        const paste = {
-            title : title,
-            content: value,
-            _id : pasteId || Date.now().toString(36) + Math.random().toString(36).substring(2), 
-            createdAt : new Date().toISOString(),
-        }
+//     const createPaste = ()=>
+//     {
+//         const paste = {
+//             title : title,
+//             content: value,
+//             _id : pasteId || Date.now().toString(36) + Math.random().toString(36).substring(2), 
+//             createdAt : new Date().toISOString(),
+//         }
 
 
-        if(pasteId)
-        {
-            //update
-            dispatch(updateToPastes(paste));
-        }
-        else
-        {
-            // create
-            dispatch(addToPastes(paste));
-        }
+//         if(pasteId)
+//         {
+//             //update
+//             dispatch(updateToPastes(paste));
+//         }
+//         else
+//         {
+//             // create
+//             dispatch(addToPastes(paste));
+//         }
 
-        // after the creation or update 
-        setTitle("");
-        setValue("");
-        setSearchParam("");
-    };
+//         // after the creation or update 
+//         setTitle("");
+//         setValue("");
+//         setSearchParam("");
+//     };
 
-    const resetPaste = () => {
-        setTitle("");
-        setValue("");
-        setSearchParam({});
-        // navigate("/");
-      };
+//     const resetPaste = () => {
+//         setTitle("");
+//         setValue("");
+//         setSearchParam({});
+//         // navigate("/");
+//       };
 
-    useEffect(() => {
-        if(pasteId)
-        {
-            const paste = pastes.find((p) => p._id === pasteId);
-         if (paste) {
-        setTitle(paste.title);
-        setValue(paste.content);
-      }
-        }
+//     useEffect(() => {
+//         if(pasteId)
+//         {
+//             const paste = pastes.find((p) => p._id === pasteId);
+//          if (paste) {
+//         setTitle(paste.title);
+//         setValue(paste.content);
+//       }
+//         }
       
-    }, [pasteId, pastes])
+//     }, [pasteId, pastes])
 
-  return (
-  <div>
-      <div className='pt-4 flex flex-row gap-6 place-content-between'>
-        <input type="text" 
-    className="p-2 rounded-xl flex pl-3 mt-2 w-[65%]" 
-         placeholder='Enter text here...'
-         value={title}
-         onChange={(e)=> setTitle(e.target.value)}
-         />
-         <button onClick={createPaste}>
-            {
-                pasteId? "UPdate Paste" : "Create my Paste"
-            }
-         </button>
-    </div>
+//   return (
+//   <div>
+//       <div className='pt-4 flex flex-row gap-6 place-content-between'>
+//         <input type="text" 
+//     className="p-2 rounded-xl flex pl-3 mt-2 w-[65%]" 
+//          placeholder='Enter text here...'
+//          value={title}
+//          onChange={(e)=> setTitle(e.target.value)}
+//          />
+//          <button onClick={createPaste}>
+//             {
+//                 pasteId? "UPdate Paste" : "Create my Paste"
+//             }
+//          </button>
+//     </div>
    
-    <div>
-        <textarea className='rounded-2xl p-5 mt-5  h-96 min-w-[599px]'
-            value={value}
-            placeholder='Enter contents here...'
-            onChange={(e)=> setValue(e.target.value)}
-        />
+//     <div>
+//         <textarea className='rounded-2xl p-5 mt-5  h-96 min-w-[599px]'
+//             value={value}
+//             placeholder='Enter contents here...'
+//             onChange={(e)=> setValue(e.target.value)}
+//         />
         
-    </div>
-  </div>
-  )
-}
+//     </div>
+//   </div>
+//   )
+// }
 
-export default Home
+// export default Home
--- paste-app/src/redux/pasteSlice.jsx
+++ paste-app/src/redux/pasteSlice.jsx
@@ -1,21 +1,18 @@
-import { createSlice } from '@reduxjs/toolkit'
-import toast from 'react-hot-toast';
+import { createSlice } from "@reduxjs/toolkit"
+import { toast } from "react-hot-toast"
 
 const initialState = {
-    pastes : localStorage.getItem('pastes')
-    ? JSON.parse(localStorage.getItem('pastes')) : []
-  
+  pastes: localStorage.getItem("pastes")
+    ? JSON.parse(localStorage.getItem("pastes"))
+    : []
 }
 
-export const pasteSlice = createSlice({
-  name: 'paste',
+const pasteSlice = createSlice({
+  name: "paste",
   initialState,
   reducers: {
-    addToPastes: (state,action) => {
-
-      // add a check -> paste already exists if exists then don't create another one
-
-      const paste = action.payload;
+    addToPastes: (state, action) => {
+      const paste = action.payload
       const index = state.pastes.findIndex((item) => item._id === paste._id)
 
       if (index >= 0) {
@@ -25,17 +22,18 @@ export const pasteSlice = createSlice({
       }
       // If the course is not in the Pastes, add it to the Pastes
       state.pastes.push(paste)
-
-      localStorage.setItem('pastes', JSON.stringify(state.pastes));
-      toast.success("Paste created successfully...")
+      
+      // Update to localstorage
+      localStorage.setItem("pastes", JSON.stringify(state.pastes))
+      // show toast
+      toast.success("Paste added")
     },
 
-    updateToPastes: (state,action) => {
-      const paste = action.payload;
-
+    updatePastes: (state, action) => {
+      const paste = action.payload
       const index = state.pastes.findIndex((item) => item._id === paste._id)
 
-       if (index >= 0) {
+      if (index >= 0) {
         // If the course is found in the Pastes, update it
         state.pastes[index] = paste
         // Update to localstorage
@@ -44,32 +42,115 @@ export const pasteSlice = createSlice({
         toast.success("Paste updated")
       }
     },
-
-    resetPaste: (state) => {
-      state.pastes = [];
-
-      localStorage.removeItem('pastes');
-      
-    },
     removeFromPastes: (state, action) => {
-      const pasteId = action.payload;
+      const pasteId = action.payload
 
-      console.log(pasteId);
-      const index = state.pastes.findIndex((item)=> item.id === pasteId);
+      console.log(pasteId)
+      const index = state.pastes.findIndex((item) => item._id === pasteId)
 
-      if(index >= 0) {
-      state.pastes.splice(index, 1);
-
-      localStorage.setItem('pastes', JSON.stringify(state.pastes));
-
-      toast.success("Paste deleted successfully");
+      if (index >= 0) {
+        // If the course is found in the Pastes, remove it
+        state.pastes.splice(index, 1)
+        // Update to localstorage
+        localStorage.setItem("pastes", JSON.stringify(state.pastes))
+        // show toast
+        toast.success("Paste deleted")
       }
-
+    },
+    resetPaste: (state) => {
+      state.pastes = []
+      // Update to localstorage
+      localStorage.removeItem("pastes")
+    },
   },
-},
 })
 
-// Action creators are generated for each case reducer function
-export const { addToPastes, updateToPastes, resetAllPaste, removeFromPastes } = pasteSlice.actions
+export const { addToPastes, removeFromPastes, updatePastes } = pasteSlice.actions
 
 export default pasteSlice.reducer
+
+
+
+
+
+
+
+
+
+
+
+// import { createSlice } from '@reduxjs/toolkit'
+// import toast from 'react-hot-toast';
+
+// const initialState = {
+//     pastes : localStorage.getItem('pastes')
+//     ? JSON.parse(localStorage.getItem('pastes')) : []
+  
+// }
+
+// export const pasteSlice = createSlice({
+//   name: 'paste',
+//   initialState,
+//   reducers: {
+//     addToPastes: (state,action) => {
+
+//       // add a check -> paste already exists if exists then don't create another one
+
+//       const paste = action.payload;
+//       const index = state.pastes.findIndex((item) => item._id === paste._id)
+
+//       if (index >= 0) {
+//         // If the course is already in the Pastes, do not modify the quantity
+//         toast.error("Paste already exist")
+//         return
+//       }
+//       // If the course is not in the Pastes, add it to the Pastes
+//       state.pastes.push(paste)
+
+//       localStorage.setItem('pastes', JSON.stringify(state.pastes));
+//       toast.success("Paste created successfully...")
+//     },
+
+//     updateToPastes: (state,action) => {
+//       const paste = action.payload;
+
+//       const index = state.pastes.findIndex((item) => item._id === paste._id)
+
+//        if (index >= 0) {
+//         // If the course is found in the Pastes, update it
+//         state.pastes[index] = paste
+//         // Update to localstorage
+//         localStorage.setItem("pastes", JSON.stringify(state.pastes))
+//         // show toast
+//         toast.success("Paste updated")
+//       }
+//     },
+
+//     resetPaste: (state) => {
+//       state.pastes = [];
+
+//       localStorage.removeItem('pastes');
+      
+//     },
+//     removeFromPastes: (state, action) => {
+//       const pasteId = action.payload;
+
+//       console.log(pasteId);
+//       const index = state.pastes.findIndex((item)=> item.id === pasteId);
+
+//       if(index >= 0) {
+//       state.pastes.splice(index, 1);
+
+//       localStorage.setItem('pastes', JSON.stringify(state.pastes));
+
+//       toast.success("Paste deleted successfully");
+//       }
+
+//   },
+// },
+// })
+
+// // Action creators are generated for each case reducer function
+// export const { addToPastes, updateToPastes, resetAllPaste, removeFromPastes } = pasteSlice.actions
+
+// export default pasteSlice.reducer
